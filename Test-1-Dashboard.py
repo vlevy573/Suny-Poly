27c02dc24e4c7913dcbb186b2fedaab3072f80a0
vlevy573 <89704624+vlevy573@users.noreply.github.com>
2025-03-09 20:43:04 -0500

    Update Test-1-Dashboard.py

--- Test-1-Dashboard.py
+++ Test-1-Dashboard.py
@@ -17,8 +17,13 @@ fig2 = px.line(df_uni, x='Year', y='Student Satisfaction (%)', title ='Student S
 # Arrange the plots in a grid layout
 col1, col2 = st.columns(2)  # Create 2 columns
 
+st.metric("Average Hours Worked", f"{df['Hours Worked'].mean():.1f} hrs")
+st.metric("Total Tasks Completed", df['Tasks Completed'].sum())
+
 with col1:
     st.plotly_chart(fig1, use_container_width=True)  # First plot in first column
+    st.metric("Retention Rate This Term", df_uni[(df_uni['Year']=='2024','Retention Rate (%)'].mean())
 
 with col2:
     st.plotly_chart(fig2, use_container_width=True)  # Second plot in second column
+    st.metric("Satisfaction Rate This Term", df_uni[(df_uni['Year']=='2024', 'Student Satisfaction (%)')].mean())
